7f42c22a1d28b067f3626260855d54d783e90dc6
Fernando Gil <gilvif@universoft.com.mx>
2025-02-14 11:19:46 -0600

    Programa base inicial de mi primer proyecto.

---
+++ src/miprimergit/MiPrimerGit.java
@@ -0,0 +1,13 @@
+package miprimergit;
+
+
+public class MiPrimerGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println ( "Hola Mundo" );
+    }
+    
+}
